a9c43bca03e34151504f6736e1bbe51d0d940ca6
super-jaba <kononoffp@gmail.com>
2022-12-05 20:52:45 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+import { useEffect } from 'react';
+
+const tg = window.Telegram.WebApp;
+
+function App() {
+
+  useEffect(() => {
+    this.ready();
+  }, []);
+
+  const onClose = () => {
+    tg.close();
+  }
+
+  return (
+    <div className="App">
+      <button onClick={onClose}>Close</button>
+    </div>
+  );
+}
+
+export default App;
