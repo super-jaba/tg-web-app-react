7f1177cfd1cffa38e5316f6ed4460184c45cbf3f
super-jaba <kononoffp@gmail.com>
2022-12-07 19:14:02 +0300

    wsdf

--- src/components/WebAppData/WebAppData.jsx
+++ src/components/WebAppData/WebAppData.jsx
@@ -4,10 +4,14 @@ import { useTelegram } from '../../hooks/useTelegram';;
 export default function WebAppData() {
   const {tg} = useTelegram();
 
+  const urlParams = new URLSearchParams(tg.initData);
+  const params = Object.fromEntries(urlParams);
+
   return (
     <div>
         <p><strong>initData Undecoded: </strong>{tg.initData}</p>
-        <p><strong>initData: </strong>{decodeURI(tg.initData)}</p>
+        <br />
+        <p><strong>initData: </strong>{params}</p>
         <br />
         <p><strong>hash:</strong> {tg.initDataUnsafe?.hash}</p>
     </div>
